f90c643e3e813d729774ea0e54c9b471dcb3ea24
Christian Curi <c.christiancuri@gmail.com>
2022-06-28 20:59:03 +0000

    chore(release): push release [ci skip]

--- dist/hooks/useComponentSize.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-export default function useComponentSize(ref: React.RefObject<HTMLElement>): {
-    width: number;
-    height: number;
-};
-//# sourceMappingURL=useComponentSize.d.ts.map
--- dist/rules/attachments.d.ts
+++
@@ -1,3 +0,0 @@
-import MarkdownIt from "markdown-it";
-export default function linksToAttachments(md: MarkdownIt): void;
-//# sourceMappingURL=attachments.d.ts.map
