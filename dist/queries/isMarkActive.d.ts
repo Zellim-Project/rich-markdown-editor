899f26718383bf64854cf177d66aaa3412f49f84
Christian Curi <c.christiancuri@gmail.com>
2022-06-29 00:20:19 +0000

    chore(release): push release [ci skip]

--- dist/queries/isMarkActive.d.ts
+++
@@ -1,5 +0,0 @@
-import { MarkType } from "prosemirror-model";
-import { EditorState } from "prosemirror-state";
-declare const isMarkActive: (type: MarkType) => (state: EditorState) => boolean;
-export default isMarkActive;
-//# sourceMappingURL=isMarkActive.d.ts.map
