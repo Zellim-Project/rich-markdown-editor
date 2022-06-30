928febfc4142f204a547107f8a2e4ed94428c72c
Christian Curi <c.christiancuri@gmail.com>
2022-06-30 13:38:17 +0000

    chore(release): push release [ci skip]

--- dist/lib/getDataTransferFiles.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-export default function getDataTransferFiles(event: Event | React.FormEvent<HTMLInputElement> | React.DragEvent<HTMLElement>): File[];
-//# sourceMappingURL=getDataTransferFiles.d.ts.map
--- dist/queries/getRowIndex.d.ts
+++
@@ -1,3 +0,0 @@
-import { CellSelection } from "prosemirror-tables";
-export default function getRowIndex(selection: CellSelection): any;
-//# sourceMappingURL=getRowIndex.d.ts.map
