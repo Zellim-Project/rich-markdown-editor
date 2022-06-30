d4487b6502952074c683622c9a5dadaeed8c7c96
Christian Curi <c.christiancuri@gmail.com>
2022-06-30 14:19:44 +0000

    chore(release): push release [ci skip]

--- dist/components/FileExtension.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-declare type Props = {
-    title: string;
-    size?: number;
-};
-export default function FileExtension(props: Props): JSX.Element;
-export {};
-//# sourceMappingURL=FileExtension.d.ts.map
--- dist/utils/urls.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function cdnPath(path: string): string;
-export declare function isInternalUrl(href: string): boolean;
-export declare function isExternalUrl(href: string): boolean;
-//# sourceMappingURL=urls.d.ts.map
