63b0892a6d283403dcfe29bea2bb256ea2e7f53c
Christian Curi <c.christiancuri@gmail.com>
2022-06-30 14:49:40 +0000

    chore(release): push release [ci skip]

--- dist/components/Input.d.ts
+++
@@ -1,3 +0,0 @@
-declare const Input: import("styled-components").StyledComponent<"input", import("styled-components").DefaultTheme, {}, never>;
-export default Input;
-//# sourceMappingURL=Input.d.ts.map
