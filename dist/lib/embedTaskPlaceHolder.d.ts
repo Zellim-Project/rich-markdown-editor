c40556ea616b24e84c716251e055a6cf52a32ed0
Christian Curi <c.christiancuri@gmail.com>
2022-07-15 17:32:57 +0000

    chore(release): 11.24.0-alpha.25

---
+++ dist/lib/embedTaskPlaceHolder.d.ts
@@ -0,0 +1,5 @@
+import { Plugin } from "prosemirror-state";
+declare const embedTaskPlaceholder: Plugin<any, any>;
+export default embedTaskPlaceholder;
+export declare function findPlaceholder(state: any, id: any): any;
+//# sourceMappingURL=embedTaskPlaceHolder.d.ts.map
