eed1b715a74ecb0fc18d1c0cb4491f7cf471cb04
Christian Curi <c.christiancuri@gmail.com>
2022-07-15 19:54:06 +0000

    chore(release): 11.24.0-alpha.29

---
+++ dist/rules/embedTask.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const markdown_it_container_1 = __importDefault(require("markdown-it-container"));
+function task(md) {
+    return markdown_it_container_1.default(md, "task", {
+        marker: "&",
+        validate: () => true,
+        render: function (tokens, idx) {
+            const { info } = tokens[idx];
+            console.log(info);
+            if (tokens[idx].nesting === 1) {
+                return `<div class="embed-task">\n`;
+            }
+            else {
+                return "</div>\n";
+            }
+        },
+    });
+}
+exports.default = task;
+//# sourceMappingURL=embedTask.js.map
