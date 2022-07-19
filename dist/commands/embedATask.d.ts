1c1d2db57b3617f58da0faa11ac7340bc8e06215
Christian Curi <c.christiancuri@gmail.com>
2022-07-19 03:06:06 +0000

    chore(release): 11.24.0-alpha.43

---
+++ dist/commands/embedATask.d.ts
@@ -0,0 +1,14 @@
+import { EditorView } from "prosemirror-view";
+import baseDictionary from "../dictionary";
+export declare type ITask = {
+    taskName: string;
+    projectName: string;
+    taskId: string;
+};
+declare const embedATask: (view: EditorView, event: Event, pos: number, options: {
+    dictionary: typeof baseDictionary;
+    embedATask?: (() => Promise<ITask>) | undefined;
+    onShowToast?: ((message: string, code: string) => void) | undefined;
+}) => void;
+export default embedATask;
+//# sourceMappingURL=embedATask.d.ts.map
