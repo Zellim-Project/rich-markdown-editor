724ff8aa2ef2d451da95b22c76b1aea5c5daa9ea
Christian Curi <c.christiancuri@gmail.com>
2022-08-11 01:26:15 +0000

    chore(release): push release [ci skip]

--- dist/lib/embedSimplePlaceHolder.d.ts
+++
@@ -1,5 +0,0 @@
-import { Plugin } from "prosemirror-state";
-declare const embedSimplePlaceHolder: Plugin<any, any>;
-export default embedSimplePlaceHolder;
-export declare function findPlaceholder(state: any, id: any): any;
-//# sourceMappingURL=embedSimplePlaceHolder.d.ts.map
