99fe663fb04d2ad1dacfa0395245c306f96f37c8
Christian Curi <c.christiancuri@gmail.com>
2022-09-08 20:19:44 +0000

    chore(release): push release [ci skip]

--- dist/plugins/Sync.d.ts
+++
@@ -1,7 +0,0 @@
-import Extension from "../lib/Extension";
-import { Plugin } from "prosemirror-state";
-export default class Sync extends Extension {
-    get name(): string;
-    get plugins(): Plugin[];
-}
-//# sourceMappingURL=Sync.d.ts.map
