f3553f81ef9e7f01c50bcf529576c1126c16e791
christiancuri <c.christiancuri@gmail.com>
2022-09-08 16:54:34 -0300

    test: pure plugin

--- src/plugins/Sync.ts
+++ src/plugins/Sync.ts
@@ -1,26 +1,33 @@
-import { ReactElement } from "react";
-import ReactDOM from "react-dom";
-import { ySyncPlugin, yCursorPlugin } from "y-prosemirror";
-import EditorCursor from "../components/EditorCursor";
+// import { ReactElement } from "react";
+// import ReactDOM from "react-dom";
+import {
+  ySyncPlugin,
+  yCursorPlugin,
+  yUndoPlugin,
+  undo,
+  redo,
+} from "y-prosemirror";
+// import EditorCursor from "../components/EditorCursor";
 import Extension from "../lib/Extension";
 import { Plugin } from "prosemirror-state";
+import { keymap } from "prosemirror-keymap";
 
-function cursorBuilder(userInfo: Record<string, string>): HTMLElement {
-  const cursor = document.createElement("span");
-  const editorCursor = EditorCursor({
-    userName: userInfo.name,
-    userColor: userInfo.color,
-  });
-  ReactDOM.render(editorCursor as ReactElement, cursor);
-  return cursor;
-}
+// function cursorBuilder(userInfo: Record<string, string>): HTMLElement {
+//   const cursor = document.createElement("span");
+//   const editorCursor = EditorCursor({
+//     userName: userInfo.name,
+//     userColor: userInfo.color,
+//   });
+//   ReactDOM.render(editorCursor as ReactElement, cursor);
+//   return cursor;
+// }
 
-const selectionBuilder = (user) => {
-  return {
-    style: `background-color: ${user.color}70`,
-    class: "ProseMirror-yjs-selection",
-  };
-};
+// const selectionBuilder = (user) => {
+//   return {
+//     style: `background-color: ${user.color}70`,
+//     class: "ProseMirror-yjs-selection",
+//   };
+// };
 
 export default class Sync extends Extension {
   get name(): string {
@@ -30,15 +37,23 @@ export default class Sync extends Extension {
   get plugins(): Plugin[] {
     return [
       ySyncPlugin(this.options.yXmlFragment),
-      yCursorPlugin(
-        this.options.yProvider.awareness,
-        {
-          cursorBuilder,
-          getSelection: (state) => state.selection,
-          selectionBuilder,
-        },
-        "cursor"
-      ),
+      yCursorPlugin(this.options.yProvider.awareness),
+      yUndoPlugin(),
+      keymap({
+        "Mod-z": undo,
+        "Mod-y": redo,
+        "Mod-Shift-z": redo,
+      }),
+      // ySyncPlugin(this.options.yXmlFragment),
+      // yCursorPlugin(
+      //   this.options.yProvider.awareness,
+      //   {
+      //     cursorBuilder,
+      //     getSelection: (state) => state.selection,
+      //     selectionBuilder,
+      //   },
+      //   "cursor"
+      // ),
     ];
   }
 }
