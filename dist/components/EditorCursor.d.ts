8b76003ffd91a020d72604e94c2d802e0b1e41dd
Christian Curi <c.christiancuri@gmail.com>
2022-09-09 17:21:02 +0000

    chore(release): push release [ci skip]

--- dist/components/EditorCursor.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-declare type Props = {
-    userName: string;
-    userColor: string;
-};
-declare const EditorCursor: React.FC<Props>;
-export default EditorCursor;
-//# sourceMappingURL=EditorCursor.d.ts.map
