e862a058ba12786acd2e34910d6f76a2de94f186
Christian Curi <c.christiancuri@gmail.com>
2022-09-22 23:16:04 +0000

    chore(release): 11.25.0-alpha.3

---
+++ dist/plugins/Sync.js
@@ -0,0 +1,38 @@
+import ReactDOM from "react-dom";
+import { ySyncPlugin, yCursorPlugin } from "y-prosemirror";
+import EditorCursor from "../components/EditorCursor";
+import Extension from "../lib/Extension";
+function cursorBuilder(userInfo) {
+    const cursor = document.createElement("span");
+    const editorCursor = EditorCursor({
+        userName: userInfo.name,
+        userColor: userInfo.color,
+    });
+    ReactDOM.render(editorCursor, cursor);
+    return cursor;
+}
+const selectionBuilder = (user) => {
+    return {
+        style: `background-color: ${user.color}70`,
+        class: "ProseMirror-yjs-selection",
+    };
+};
+export default class Sync extends Extension {
+    get name() {
+        return "sync";
+    }
+    get plugins() {
+        if (this.options.yXmlFragment && this.options.yProvider) {
+            return [
+                ySyncPlugin(this.options.yXmlFragment),
+                yCursorPlugin(this.options.yProvider.awareness, {
+                    cursorBuilder,
+                    getSelection: (state) => state.selection,
+                    selectionBuilder,
+                }, "cursor"),
+            ];
+        }
+        return [];
+    }
+}
+//# sourceMappingURL=Sync.js.map
