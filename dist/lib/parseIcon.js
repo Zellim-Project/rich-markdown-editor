137155efb7abd108dd53a4769b5daa3aface3707
Christian Curi <c.christiancuri@gmail.com>
2022-10-18 20:38:10 +0000

    chore(release): 11.25.0-alpha.15

---
+++ dist/lib/parseIcon.js
@@ -0,0 +1,17 @@
+export const getIcon = (name, mimetype) => {
+    if (mimetype.includes("audio")) {
+        return `/file-mp3.svg`;
+    }
+    if (name?.split(".").length > 1) {
+        return `/file-${name.split(".").reverse()[0]}.svg`;
+    }
+    if (mimetype?.split("/").length > 1) {
+        return `/file-${mimetype
+            .split("/")
+            .reverse()[0]
+            .split(".")
+            .reverse()[0]}.svg`;
+    }
+    return `/file-no-extension.svg`;
+};
+//# sourceMappingURL=parseIcon.js.map
---
+++ dist/nodes/linkDocument.js
@@ -0,0 +1,109 @@
+import { wrappingInputRule } from "prosemirror-inputrules";
+import { Plugin } from "prosemirror-state";
+import toggleWrap from "../commands/toggleWrap";
+import { Union } from "../lib/icons";
+import * as React from "react";
+import { linkDocumentPlaceholder } from "../lib/embedSimplePlaceHolder";
+import Node from "./Node";
+import linkDocumentRules from "../rules/linkDocument";
+export default class EmbedTask extends Node {
+    constructor() {
+        super(...arguments);
+        this.component = (props) => {
+            const { docId, docName, icon } = props.node.attrs;
+            const { openDocument } = this.editor.props;
+            return (React.createElement("div", { contentEditable: false, className: "embed-block", onClick: () => openDocument?.({ docId }) },
+                React.createElement("div", { className: "mentioned-icon my-2 ms-2" }, icon ? React.createElement("span", { className: "me-3 emoji" }, icon) : React.createElement(Union, null)),
+                React.createElement("div", { className: "mentioned-info" },
+                    React.createElement("p", { className: "doc-id" }, docId),
+                    React.createElement("p", { className: "title" }, docName))));
+        };
+    }
+    get name() {
+        return "link_doc";
+    }
+    get schema() {
+        return {
+            attrs: {
+                docId: {
+                    default: "",
+                },
+                docName: {
+                    default: "",
+                },
+                icon: {
+                    default: "",
+                },
+            },
+            content: "block+",
+            group: "block",
+            defining: true,
+            draggable: false,
+            parseDOM: [
+                {
+                    tag: "div.embed-block",
+                    preserveWhitespace: "full",
+                    contentElement: "div.info",
+                    getAttrs: (dom) => ({
+                        docName: dom.getElementsByClassName("title")[0].textContent,
+                        docId: dom.getElementsByClassName("doc-id")[0].textContent,
+                        icon: dom.getElementsByClassName("emoji")[0].textContent,
+                    }),
+                },
+            ],
+            toDOM: (node) => {
+                return [
+                    "div",
+                    { class: "embed-block" },
+                    ["p", { ...node.attrs, contentEditable: false }],
+                ];
+            },
+        };
+    }
+    commands({ type }) {
+        return (attrs) => toggleWrap(type, attrs);
+    }
+    get rulePlugins() {
+        return [linkDocumentRules];
+    }
+    inputRules({ type }) {
+        return [wrappingInputRule(/^)-)-)-$/, type)];
+    }
+    toMarkdown(state, node) {
+        state.ensureNewLine();
+        state.write("\\\\");
+        state.ensureNewLine();
+        state.write(")-)-)-");
+        state.write("[" +
+            state.esc(node.attrs.docId) +
+            "]" +
+            "(" +
+            state.esc(node.attrs.docName) +
+            "&-&" +
+            state.esc(node.attrs.icon) +
+            ")");
+        state.ensureNewLine();
+        state.write(")-)-)-");
+        state.closeBlock(node);
+    }
+    parseMarkdown() {
+        return {
+            block: "link_doc",
+            getAttrs: (token) => {
+                const file_regex = /\[(?<docId>[^]*?)\]\((?<docName>[^]*?)\)/g;
+                const result = file_regex.exec(token.info);
+                const [docName, icon] = result?.[2]?.split("&-&") || [];
+                const docId = result?.[1] || "";
+                return {
+                    docId: result ? docId : null,
+                    docName: result ? docName : null,
+                    icon: result ? icon : null,
+                };
+            },
+        };
+    }
+    get plugins() {
+        return [linkDocumentPlaceholder, new Plugin({})];
+    }
+}
+//# sourceMappingURL=linkDocument.js.map
