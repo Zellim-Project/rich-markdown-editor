3c1253d51e1146ed3cf28e4b627c3b680b669ee7
Christian Curi <c.christiancuri@gmail.com>
2022-10-20 20:45:07 +0000

    chore(release): push release [ci skip]

--- dist/lib/embedSimplePlaceHolder.d.ts
+++
@@ -1,7 +0,0 @@
-import { Plugin } from "prosemirror-state";
-export declare const embedTaskPlaceholder: Plugin<any, any>;
-export declare const embedProjectPlaceholder: Plugin<any, any>;
-export declare const mentionDocumentPlaceholder: Plugin<any, any>;
-export declare const linkDocumentPlaceholder: Plugin<any, any>;
-export declare function findPlaceholder(state: any, id: any, type: any): any;
-//# sourceMappingURL=embedSimplePlaceHolder.d.ts.map
--- dist/nodes/ReactNode.js
+++
@@ -1,4 +0,0 @@
-import Node from "./Node";
-export default class ReactNode extends Node {
-}
-//# sourceMappingURL=ReactNode.js.map
