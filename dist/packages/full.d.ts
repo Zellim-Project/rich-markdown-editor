f1456023f894b74ff1792a6c6531fa4d87c463c0
Christian Curi <c.christiancuri@gmail.com>
2022-10-26 20:21:14 +0000

    chore(release): push release [ci skip]

--- dist/packages/full.d.ts
+++
@@ -1,7 +0,0 @@
-import TemplatePlaceholder from "../marks/Placeholder";
-import Heading from "../nodes/Heading";
-import Table from "../nodes/Table";
-import Folding from "../plugins/Folding";
-declare const fullPackage: (typeof TemplatePlaceholder | typeof Heading | typeof Table | typeof Folding | typeof import("../marks/Bold").default | typeof import("../marks/Code").default | typeof import("../marks/Italic").default | typeof import("../marks/Link").default | typeof import("../marks/Strikethrough").default | typeof import("../marks/Underline").default | typeof import("../nodes/Paragraph").default)[];
-export default fullPackage;
-//# sourceMappingURL=full.d.ts.map
