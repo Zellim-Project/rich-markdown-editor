41e57d9ed18c95c82f66a28c0e8952d53b6f96dc
Christian Curi <c.christiancuri@gmail.com>
2022-11-02 23:20:38 +0000

    chore(release): push release [ci skip]

--- dist/components/ToolbarSeparator.d.ts
+++
@@ -1,3 +0,0 @@
-declare const Separator: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {}, never>;
-export default Separator;
-//# sourceMappingURL=ToolbarSeparator.d.ts.map
