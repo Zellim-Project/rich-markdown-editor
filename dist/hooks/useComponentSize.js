deff2554e2b72c9cdc846955106c3aeb32c48844
Christian Curi <c.christiancuri@gmail.com>
2022-11-02 23:38:48 +0000

    chore(release): 11.25.0-alpha.31

---
+++ dist/hooks/useComponentSize.js
@@ -0,0 +1,22 @@
+import ResizeObserver from "resize-observer-polyfill";
+import { useState, useEffect } from "react";
+export default function useComponentSize(ref) {
+    const [size, setSize] = useState({
+        width: 0,
+        height: 0,
+    });
+    useEffect(() => {
+        const sizeObserver = new ResizeObserver(entries => {
+            entries.forEach(({ target }) => {
+                if (size.width !== target.clientWidth ||
+                    size.height !== target.clientHeight) {
+                    setSize({ width: target.clientWidth, height: target.clientHeight });
+                }
+            });
+        });
+        sizeObserver.observe(ref.current);
+        return () => sizeObserver.disconnect();
+    }, [ref]);
+    return size;
+}
+//# sourceMappingURL=useComponentSize.js.map
