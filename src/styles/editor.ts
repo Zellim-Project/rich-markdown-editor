832c005da801062aed8b1c9a1b2d1762c83b2ce0
Eduardo Lopez <eduardidn@gmail.com>
2022-11-03 09:37:49 -0500

    updating loader

--- src/styles/editor.ts
+++ src/styles/editor.ts
@@ -79,18 +79,6 @@ export const StyledEditor = styled("div")<{
     clear: initial;
   }
 
-  .loader {
-    border: 5px solid #f3f3f3;
-    border-radius: 50%;
-    border-top: 5px solid #3498db;
-    width: 2.5em;
-    height: 2.5em;
-    margin: 80px auto;
-    position: relative;
-    -webkit-animation: spin 2s linear infinite; /* Safari */
-    animation: spin 2s linear infinite;
-  }
-
   @keyframes spin {
     0% { transform: rotate(0deg); }
     100% { transform: rotate(360deg); }
