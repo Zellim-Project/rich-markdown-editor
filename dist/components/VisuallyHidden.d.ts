698e951b6434aab857aa5f2eb1abf07511f3e40f
Christian Curi <c.christiancuri@gmail.com>
2022-11-07 20:12:23 +0000

    chore(release): 11.25.0-alpha.37

---
+++ dist/components/VisuallyHidden.d.ts
@@ -0,0 +1,3 @@
+declare const VisuallyHidden: import("styled-components").StyledComponent<"span", import("styled-components").DefaultTheme, {}, never>;
+export default VisuallyHidden;
+//# sourceMappingURL=VisuallyHidden.d.ts.map
