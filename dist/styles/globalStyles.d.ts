26c0286fe131b8b6002b1d1d3753aff6f55c87ce
Christian Curi <c.christiancuri@gmail.com>
2022-11-09 20:34:38 +0000

    chore(release): 11.25.0-alpha.52

---
+++ dist/styles/globalStyles.d.ts
@@ -0,0 +1,6 @@
+declare type Props = {
+    useCursorPointer?: boolean;
+};
+declare const _default: import("styled-components").GlobalStyleComponent<Props, import("styled-components").DefaultTheme>;
+export default _default;
+//# sourceMappingURL=globalStyles.d.ts.map
