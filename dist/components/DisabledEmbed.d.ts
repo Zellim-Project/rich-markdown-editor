12a639ed492945a7f9b76dde7792990540439450
Christian Curi <c.christiancuri@gmail.com>
2022-11-09 20:10:35 +0000

    chore(release): push release [ci skip]

--- dist/components/DisabledEmbed.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { DefaultTheme, ThemeProps } from "styled-components";
-export default function DisabledEmbed(props: any & ThemeProps<DefaultTheme>): JSX.Element;
-//# sourceMappingURL=DisabledEmbed.d.ts.map
