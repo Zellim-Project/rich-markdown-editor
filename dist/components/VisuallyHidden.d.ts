516c2b46a182b399dc472aae8299225f257b918f
Christian Curi <c.christiancuri@gmail.com>
2022-11-21 22:05:19 +0000

    chore(release): push release [ci skip]

--- dist/components/VisuallyHidden.d.ts
+++
@@ -1,3 +0,0 @@
-declare const VisuallyHidden: import("styled-components").StyledComponent<"span", import("styled-components").DefaultTheme, {}, never>;
-export default VisuallyHidden;
-//# sourceMappingURL=VisuallyHidden.d.ts.map
