59c39bb73020aca06e0f94f720868ea498cb67db
Christian Curi <c.christiancuri@gmail.com>
2023-01-20 17:14:47 +0000

    chore(release): 11.25.1-beta.0

---
+++ dist/plugins/SmartText.d.ts
@@ -0,0 +1,7 @@
+import { InputRule } from "prosemirror-inputrules";
+import Extension from "../lib/Extension";
+export default class SmartText extends Extension {
+    get name(): string;
+    inputRules(): InputRule[];
+}
+//# sourceMappingURL=SmartText.d.ts.map
