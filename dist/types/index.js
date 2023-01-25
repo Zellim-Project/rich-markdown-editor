5ff2264c90544599e9081984e27acdb150960f4a
Christian Curi <c.christiancuri@gmail.com>
2023-01-25 23:41:01 +0000

    chore(release): push release [ci skip]

--- dist/types/index.js
+++
@@ -1,6 +0,0 @@
-export var ToastType;
-(function (ToastType) {
-    ToastType["Error"] = "error";
-    ToastType["Info"] = "info";
-})(ToastType || (ToastType = {}));
-//# sourceMappingURL=index.js.map
