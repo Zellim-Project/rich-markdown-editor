bdd28378e84c221deef2f3966713cc29c32f12fa
Christian Curi <c.christiancuri@gmail.com>
2023-01-27 17:34:54 +0000

    chore(release): push release [ci skip]

--- dist/components/ToolbarButton.d.ts
+++
@@ -1,7 +0,0 @@
-declare type Props = {
-    active?: boolean;
-    disabled?: boolean;
-};
-declare const _default: import("styled-components").StyledComponent<"button", import("styled-components").DefaultTheme, Props, never>;
-export default _default;
-//# sourceMappingURL=ToolbarButton.d.ts.map
--- dist/rules/emoji.js
+++
@@ -1,9 +0,0 @@
-import nameToEmoji from "gemoji/name-to-emoji.json";
-import emojiPlugin from "markdown-it-emoji";
-export default function emoji(md) {
-    return emojiPlugin(md, {
-        defs: nameToEmoji,
-        shortcuts: {},
-    });
-}
-//# sourceMappingURL=emoji.js.map
