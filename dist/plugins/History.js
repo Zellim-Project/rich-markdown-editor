113ee21d4dd9bf9307398fec57ef79478ddc1cc2
Christian Curi <c.christiancuri@gmail.com>
2023-01-27 17:21:34 +0000

    chore(release): 11.25.1-beta.28

---
+++ dist/plugins/History.js
@@ -0,0 +1,20 @@
+import { undoInputRule } from "prosemirror-inputrules";
+import { yUndoPlugin, undo, redo } from "y-prosemirror";
+import Extension from "../lib/Extension";
+export default class History extends Extension {
+    get name() {
+        return "history";
+    }
+    keys() {
+        return {
+            "Mod-z": undo,
+            "Mod-y": redo,
+            "Shift-Mod-z": redo,
+            Backspace: undoInputRule,
+        };
+    }
+    get plugins() {
+        return [yUndoPlugin()];
+    }
+}
+//# sourceMappingURL=History.js.map
