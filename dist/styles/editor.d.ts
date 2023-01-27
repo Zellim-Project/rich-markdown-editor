e906f108c9386980450714fc4b53c3ddc08c5161
Christian Curi <c.christiancuri@gmail.com>
2023-01-27 18:33:53 +0000

    chore(release): push release [ci skip]

--- dist/styles/editor.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const StyledEditor: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {
-    rtl: boolean;
-    readOnly?: boolean | undefined;
-    readOnlyWriteCheckboxes?: boolean | undefined;
-}, never>;
-//# sourceMappingURL=editor.d.ts.map
