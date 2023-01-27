dd8941704e81af756885135c7e07211260fb2571
Christian Curi <c.christiancuri@gmail.com>
2023-01-27 15:02:21 +0000

    chore(release): 11.25.1-beta.23

---
+++ dist/plugins/Sync.d.ts
@@ -0,0 +1,7 @@
+import Extension from "../lib/Extension";
+import { Plugin } from "prosemirror-state";
+export default class Sync extends Extension {
+    get name(): string;
+    get plugins(): Plugin[];
+}
+//# sourceMappingURL=Sync.d.ts.map
