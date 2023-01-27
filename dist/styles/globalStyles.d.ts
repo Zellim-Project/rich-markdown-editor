e3d533b9268334ba91994f459a012aa82833cc7c
Christian Curi <c.christiancuri@gmail.com>
2023-01-27 03:54:10 +0000

    chore(release): push release [ci skip]

--- dist/styles/globalStyles.d.ts
+++
@@ -1,6 +0,0 @@
-declare type Props = {
-    useCursorPointer?: boolean;
-};
-declare const _default: import("styled-components").GlobalStyleComponent<Props, import("styled-components").DefaultTheme>;
-export default _default;
-//# sourceMappingURL=globalStyles.d.ts.map
