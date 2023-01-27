cee45827db0836f6cbd6a35370772e7709990fc8
Christian Curi <c.christiancuri@gmail.com>
2023-01-27 17:09:38 +0000

    chore(release): push release [ci skip]

--- dist/commands/toggleList.d.ts
+++
@@ -1,4 +0,0 @@
-import { NodeType } from "prosemirror-model";
-import { EditorState, Transaction } from "prosemirror-state";
-export default function toggleList(listType: NodeType, itemType: NodeType): (state: EditorState, dispatch: (tr: Transaction) => void) => any;
-//# sourceMappingURL=toggleList.d.ts.map
