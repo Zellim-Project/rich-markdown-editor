b380f4d3ee3fb0738756520e13aefcb51bad1451
Christian Curi <c.christiancuri@gmail.com>
2023-01-27 17:27:52 +0000

    chore(release): 11.25.1-beta.29

---
+++ dist/marks/Bold.d.ts
@@ -0,0 +1,35 @@
+import Mark from "./Mark";
+export default class Bold extends Mark {
+    get name(): string;
+    get schema(): {
+        parseDOM: ({
+            tag: string;
+            style?: undefined;
+            getAttrs?: undefined;
+        } | {
+            style: string;
+            getAttrs: (value: any) => boolean;
+            tag?: undefined;
+        })[];
+        toDOM: () => string[];
+    };
+    inputRules({ type }: {
+        type: any;
+    }): import("prosemirror-inputrules").InputRule[];
+    keys({ type }: {
+        type: any;
+    }): {
+        "Mod-b": import("prosemirror-state").Command;
+        "Mod-B": import("prosemirror-state").Command;
+    };
+    get toMarkdown(): {
+        open: string;
+        close: string;
+        mixable: boolean;
+        expelEnclosingWhitespace: boolean;
+    };
+    parseMarkdown(): {
+        mark: string;
+    };
+}
+//# sourceMappingURL=Bold.d.ts.map
