10acbe6830e50bc0830b6b371160254c8b5d9d7f
Christian Curi <c.christiancuri@gmail.com>
2023-01-27 03:54:10 +0000

    chore(release): 11.25.1-beta.19

---
+++ dist/lib/getMarkAttrs.d.ts
@@ -0,0 +1,4 @@
+import { EditorState } from "prosemirror-state";
+import Node from "../nodes/Node";
+export default function getMarkAttrs(state: EditorState, type: Node): import("prosemirror-model").Attrs;
+//# sourceMappingURL=getMarkAttrs.d.ts.map
