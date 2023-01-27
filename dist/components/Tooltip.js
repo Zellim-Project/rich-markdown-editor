0e3821980ec29c97771cb1b07f0ad8098fad2c36
Christian Curi <c.christiancuri@gmail.com>
2023-01-27 23:20:30 +0000

    chore(release): push release [ci skip]

--- dist/components/Tooltip.js
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-export default function Tooltip({ tooltip, children }) {
-    return React.createElement("span", { title: tooltip }, children);
-}
-//# sourceMappingURL=Tooltip.js.map
