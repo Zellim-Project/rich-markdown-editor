8b345a8685984c2da695162b78f614172488b8aa
Christian Curi <c.christiancuri@gmail.com>
2023-02-20 03:04:28 +0000

    chore(release): push release [ci skip]

--- dist/commands/toggleBlockType.d.ts
+++
@@ -1,5 +0,0 @@
-import { NodeType } from "prosemirror-model";
-import { EditorState } from "prosemirror-state";
-import { Dispatch } from "../types";
-export default function toggleBlockType(type: NodeType, toggleType: NodeType, attrs?: {}): (state: EditorState, dispatch?: Dispatch | undefined) => boolean;
-//# sourceMappingURL=toggleBlockType.d.ts.map
