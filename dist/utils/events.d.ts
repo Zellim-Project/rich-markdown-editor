b802dea017335f8d77d4cdf91cb57ed48567a4b7
Christian Curi <c.christiancuri@gmail.com>
2023-02-21 02:54:18 +0000

    chore(release): push release [ci skip]

--- dist/utils/events.d.ts
+++
@@ -1,9 +0,0 @@
-export default class EventEmitter {
-    private listeners;
-    addListener(name: string, callback: (data: unknown) => unknown): void;
-    removeListener(name: string, callback: (data: unknown) => unknown): void;
-    on: (name: string, callback: (data: unknown) => unknown) => void;
-    off: (name: string, callback: (data: unknown) => unknown) => void;
-    emit(name: string, data?: unknown): void;
-}
-//# sourceMappingURL=events.d.ts.map
