a76be21bcb4b58ae1c59247c6b3faf500672f9a8
Christian Curi <c.christiancuri@gmail.com>
2023-02-21 00:22:04 +0000

    chore(release): 11.25.0-alpha.71

---
+++ dist/commands/splitHeading.d.ts
@@ -0,0 +1,5 @@
+import { NodeType } from "prosemirror-model";
+import { EditorState } from "prosemirror-state";
+import { Dispatch } from "../types";
+export default function splitHeading(type: NodeType): (state: EditorState, dispatch: Dispatch) => boolean;
+//# sourceMappingURL=splitHeading.d.ts.map
